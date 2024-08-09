52796b080fe7370c20dc13f9001ec13587d3a8d4
Aniruddha Paik <aniruddhapaik893@gmail.com>
2024-08-09 18:55:16 +0530

    New work OOP assignment, Inheritance & Polymorphism, Diagrams

---
+++ OOP Class Exercises/polyinherit.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class Human {};
+class Employee {};
+class FullTimeEmployee {};
+class PartTimeEmployee {};
+// ... continue and define a hierarchy and relationship
+// Draw diagrams, lookup the many types of diagrams
+
+int main() {
+
+    return 0;
+}
