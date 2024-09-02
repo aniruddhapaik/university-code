3dea63885f73d4565ee1e72558817647b819edf3
Aniruddha Paik <aniruddhapaik893@gmail.com>
2024-09-03 01:54:21 +0530

    started off with some boilerplate code for Priority Queue

---
+++ DSA Class Exercises/Data Structures/Different Types of Queues/Priority Queue/mainiopq.cpp
@@ -0,0 +1,10 @@
+#include "../../input_utils.h"
+#include "priorityqueuewitharray.cpp"
+
+void printMenu() {}
+
+int main() {
+	printMenu();
+
+	return 0;
+}
---
+++ DSA Class Exercises/Data Structures/Different Types of Queues/Priority Queue/priorityqueuewitharray.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+class PriorityQueueWithArray {
+private:
+
+public:
+	PriorityQueueWithArray() {}
+};
