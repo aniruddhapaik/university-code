afb73d7b6360039ed525c2d58ca6b19d0243cb11
Aniruddha Paik <aniruddhapaik893@gmail.com>
2025-03-19 01:08:01 +0530

    done with round robin algorithm

---
+++ Operating System/9 roundrobin.cpp
@@ -0,0 +1,66 @@
+#include <chrono>
+#include <iostream>
+#include <queue>
+#include <thread>
+#include <vector>
+
+struct Process {
+  int id;
+  int burstTime;
+  int remainingTime;
+};
+
+void roundRobinScheduling(
+    std::vector<Process>& processes, int timeQuantum) {
+  std::queue<Process> processQueue;
+  for (auto& process : processes) {
+    processQueue.push(process);
+  }
+
+  int currentTime = 0;
+  while (!processQueue.empty()) {
+    Process currentProcess = processQueue.front();
+    processQueue.pop();
+
+    if (currentProcess.remainingTime > timeQuantum) {
+      std::cout << std::string("[T+" + std::to_string(currentTime) + "ms] P" +
+                               std::to_string(currentProcess.id) +
+                               " is executing for " +
+                               std::to_string(timeQuantum) + "ms\n");
+      //<< std::endl;
+      currentTime += timeQuantum;
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(timeQuantum));
+
+      currentProcess.remainingTime -= timeQuantum;
+      processQueue.push(currentProcess);
+    } else {
+      std::cout << std::string(
+          "[T+" + std::to_string(currentTime) + "ms] P" +
+          std::to_string(currentProcess.id) + " is executing for " +
+          std::to_string(currentProcess.remainingTime) + "ms\n");
+      //<< std::endl;
+      currentTime += currentProcess.remainingTime;
+
+      std::this_thread::sleep_for(
+          std::chrono::milliseconds(currentProcess.remainingTime));
+
+      currentProcess.remainingTime = 0;
+      std::cout << std::string("[T+" + std::to_string(currentTime) + "ms] --- P" +
+                               std::to_string(currentProcess.id) +
+                               " finished at time " +
+                               std::to_string(currentTime) + '\n')
+                << std::endl;
+    }
+  }
+}
+
+int main() {
+  std::vector<Process> processes = {
+      {1, 100, 100}, {2, 250, 250}, {3, 300, 300}};
+  int timeQuantum = 20;
+
+  roundRobinScheduling(processes, timeQuantum);
+
+  return 0;
+}
