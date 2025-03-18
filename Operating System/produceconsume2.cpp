0b4b000c63cd3aeac69850ba0d343d29105afb2b
Aniruddha Paik <aniruddhapaik893@gmail.com>
2025-03-18 15:41:42 +0530

    procucer consumer problem with nice output

--- Operating System/produceconsume2.cpp
+++ Operating System/produceconsume2.cpp
@@ -1,7 +1,9 @@
+#include <chrono>
 #include <condition_variable>
 #include <iostream>
 #include <mutex>
 #include <queue>
+#include <random>
 #include <thread>
 
 class Buffer {
@@ -11,16 +13,24 @@ class Buffer {
   std::mutex mtx;
   std::condition_variable not_full;
   std::condition_variable not_empty;
+  std::chrono::time_point<std::chrono::steady_clock> starttime;
 
  public:
-  Buffer(int size) : max_size(size) {}
+  Buffer(int size, std::chrono::time_point<std::chrono::steady_clock> starttime)
+      : max_size(size), starttime(starttime) {}
 
   void produce(int item) {
     std::unique_lock<std::mutex> lock(mtx);
     not_full.wait(lock, [this] { return queue.size() < max_size; });
 
     queue.push(item);
-    std::cout << "Produced: " << item << std::endl;
+    auto now = std::chrono::steady_clock::now();
+    auto elapsed =
+        std::chrono::duration_cast<std::chrono::milliseconds>(now - starttime)
+            .count();
+    std::cout << std::string("[T " + std::to_string(elapsed) + " ms]" +
+                             " + Produced: " + std::to_string(item))
+              << std::endl;
 
     lock.unlock();
     not_empty.notify_one();
@@ -32,7 +42,13 @@ class Buffer {
 
     int item = queue.front();
     queue.pop();
-    std::cout << "Consumed: " << item << std::endl;
+    auto now = std::chrono::steady_clock::now();
+    auto elapsed =
+        std::chrono::duration_cast<std::chrono::milliseconds>(now - starttime)
+            .count();
+    std::cout << std::string("[T " + std::to_string(elapsed) + " ms]" +
+                             " - Consumed: " + std::to_string(item))
+              << std::endl;
 
     lock.unlock();
     not_full.notify_one();
@@ -48,14 +64,22 @@ void producer(Buffer &buffer, int count) {
 }
 
 void consumer(Buffer &buffer, int count) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(150, 500);
+
   for (int i = 0; i < count; ++i) {
     buffer.consume();
-    std::this_thread::sleep_for(std::chrono::milliseconds(150));
+    int sleeptime = dis(gen);
+    std::this_thread::sleep_for(std::chrono::milliseconds(sleeptime));
+    std::cout << std::string("Resuming after " + std::to_string(sleeptime) +
+                             " ms ");
   }
 }
 
 int main() {
-  Buffer buffer(5);  // Buffer with capacity 5
+  auto starttime = std::chrono::steady_clock::now();
+  Buffer buffer(5, starttime);  // Buffer with capacity 5
   int items_to_produce = 20;
 
   std::thread producer_thread(producer, std::ref(buffer), items_to_produce);
