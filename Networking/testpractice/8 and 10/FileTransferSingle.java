ccca151a07d23f43207cce2f056474f4fcd5190f
Aniruddha Paik <aniruddhapaik893@gmail.com>
2025-04-11 09:01:05 +0530

    updated, modified, simplified programs

--- Networking/testpractice/8 and 10/FileTransferSingle.java
+++ Networking/testpractice/8 and 10/FileTransferSingle.java
@@ -4,70 +4,59 @@ import java.util.Scanner;
 
 public class FileTransferSingle {
   private static final int PORT = 5000;
-  private static final String FILE_TO_SEND = "D:\\Games\\sample.txt";
-  private static final String FILE_TO_RECEIVE = "D:\\Games\\received.txt";
+  private static final String READ_PATH = "D:\\Games\\sample.txt";
+  private static final String RECEIVE_PATH = "D:\\Games\\received.txt";
 
   public static void main(String[] args) {
+    System.out.println("Choose mode: \n1. Send \n2. Receive");
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Choose mode: \n1. Sender \n2. Receiver");
     int choice = scanner.nextInt();
-    
-    if (choice == 1) {
-      startSender();
-    } else if (choice == 2) {
-      startReceiver();
-    } else {
-      System.out.println("Invalid choice. Exiting...");
-    }
-
+    if (choice == 1) { sender(); }
+    else { receiver(); }
     scanner.close();
   }
-  
-  public static void startSender() {
-    try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-      System.out.println("Waiting for receiver...");
-      Socket socket = serverSocket.accept();
-      System.out.println("Receiver connected!");
 
-      File file = new File(FILE_TO_SEND);
+  public static void sender() {
+    try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+      System.out.println("S: waiting for receiver to connect...");
+      Socket socket = serverSocket.accept();
+      System.out.println("S: receiver connected.");
+
+      File file = new File(READ_PATH);
       FileInputStream fileInputStream = new FileInputStream(file);
       OutputStream outputStream = socket.getOutputStream();
       byte[] buffer = new byte[4096];
       int bytesRead;
 
-      while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+      while((bytesRead = fileInputStream.read(buffer)) != -1) {
         outputStream.write(buffer, 0, bytesRead);
       }
 
-      System.out.println("File sent successfully!");
+      System.out.println("S: File sent successfully.");
       fileInputStream.close();
       outputStream.close();
       socket.close();
       serverSocket.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    } catch (IOException e) { e.printStackTrace(); }
   }
-  
-  public static void startReceiver() {
+
+  public static void receiver() {
     try (Socket socket = new Socket("localhost", PORT)) {
-      System.out.println("Connected to sender!");
+      System.out.println("R: Connected to the sender server.");
 
       InputStream inputStream = socket.getInputStream();
-      FileOutputStream fileOutputStream = new FileOutputStream(FILE_TO_RECEIVE);
+      FileOutputStream fileOutputStream = new FileOutputStream(RECEIVE_PATH);
       byte[] buffer = new byte[4096];
-      int bytesRead;
+      int bytesRead = 0;
 
-      while ((bytesRead = inputStream.read(buffer)) != -1) {
+      while((bytesRead = inputStream.read(buffer)) != -1) {
         fileOutputStream.write(buffer, 0, bytesRead);
       }
 
-      System.out.println("File received successfully!");
+      System.out.println("R: file received and written successfully");
+      socket.close();
       fileOutputStream.close();
       inputStream.close();
-      socket.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    } catch (IOException e) { e.printStackTrace(); }
   }
 }
