7d06a7cd55d220f8470754fe80eeb825bc06af69
Aniruddha Paik <aniruddhapaik893@gmail.com>
2025-04-15 08:01:36 +0530

    practicing OS problems

--- Operating System/testpractice/filecpmv.cpp
+++ Operating System/testpractice/filecpmv.cpp
@@ -1,47 +1,41 @@
+#include <iostream>
 #include <filesystem>
 #include <fstream>
-#include <iostream>
-#include <limits>
 
 namespace fs = std::filesystem;
 
+const std::string filename = "example.txt";
+const std::string copydest = "copied.txt";
+const std::string movedest = "moved.txt"; 
+
 void copyFile() {
   try {
-    std::cout << "* Copying file* " << std::endl;
-    fs::path source = "example.txt";
-    if (fs::exists(source)) {
-      std::cout << "Source: " << source.string() << " exists\n" << std::endl;
-    } else { std::ofstream file(source); file.close(); }
+    std::cout << "\nChecking source file" << std::endl;
+    fs::path source = filename;
+    if (not fs::exists(source)) { std::ofstream file(filename); file.close(); }
+    if (fs::exists(source)) { std::cout << "source file exists." << std::endl; }
 
-    fs::path destination = "copied.txt";
-    if (not fs::exists(destination)) {
-      std::cout << "Destination: " << destination.string()
-                << " does not exist" << std::endl;
-      fs::copy_file(source, destination);
-      std::cout << "File copied successfully." << std::endl;
-    } else { return; }
-  } catch (const fs::filesystem_error& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-  }
+    std::cout << "Copying source file" << std::endl;
+    fs::path destination = copydest;
+    if (fs::exists(destination)) { fs::remove(destination); }
+    fs::copy_file(source, destination);
+    std::cout << "File copied successfully" << std::endl;
+  } catch (fs::filesystem_error& e) { std::cerr << "Error: " << e.what() << std::endl; }
 }
 
 void moveFile() {
   try {
-    std::cout << "* Moving file *\n";
-    fs::path source = "example.txt";
-    if (fs::exists(source)) {
-      std::cout << "Source: " << source.string() << " exists\n" << std::endl;
-    } else { return; }
+    std::cout << "\nChecking source file" << std::endl;
+    fs::path source = filename;
+    if (not fs::exists(source)) { std::ofstream file(filename); file.close(); }
+    if (fs::exists(source)) { std::cout << "source file exists." << std::endl; }
 
-    fs::path destination = "moved.txt";
-    if (not fs::exists(destination)) {
-      std::cout << "Destination: " << destination.string() << " does not exist\nmoving...";
-      fs::rename(source, destination);
-      std::cout << "File moved successfully." << std::endl;
-    } else { return; }
-  } catch (const fs::filesystem_error& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-  }
+    std::cout << "Moving file" << std::endl;
+    fs::path destination = movedest;
+    if (fs::exists(destination)) { fs::remove(destination); }
+    fs::rename(source, destination);
+    std::cout << "Moved file successfully" << std::endl;
+  } catch (fs::filesystem_error& e) { std::cerr << "Error: " << e.what() << std::endl; }
 }
 
 int main() {
